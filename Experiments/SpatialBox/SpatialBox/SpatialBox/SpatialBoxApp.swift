455ce44f7db54c78ac8cb7b9050383dc35263f6f
SEHWAN YOO <madebyrsh.dev@gmail.com>
2026-06-06 01:10:38 +0900

    Initialize SpatialBox experiment [SpatialBox 실험 프로젝트 생성]

---
+++ Experiments/SpatialBox/SpatialBox/SpatialBox/SpatialBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialBoxApp.swift
+//  SpatialBox
+//
+//  Created by Shayne Ryu on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SpatialBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
